ed6ce7eb9280f31bc0f7cb259fde1f16e691b081
vlcoscar8 <vlcoscar8@gmail.com>
2022-07-18 18:23:20 +0200

    use user detail by id custom hook

---
+++ src/customHook/useUserDetailById.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { getUserDetail } from "../state/context/services/authContext.services";
+
+export const useUserDetailById = (userId) => {
+    const [userDetail, setUserDetail] = useState();
+
+    useEffect(() => {
+        getUserDetail(userId).then((res) => setUserDetail(res));
+    }, [userId]);
+
+    return userDetail;
+};
+
+
