22bc9089050cffa0552ad3ea2f5dc92bf8699eae
Francisco José Valiente <gunko360@gmail.com>
2022-07-19 15:42:41 +0200

    commit before working in tags lists

--- src/pages/Home/components/TravelsUsersContainer/TravelsUsersContainer.jsx
+++ src/pages/Home/components/TravelsUsersContainer/TravelsUsersContainer.jsx
@@ -5,12 +5,15 @@ import RightArrowIcon from "../../../../assets/icons/right-arrow.png";
 import LeftArrowIcon from "../../../../assets/icons/left-arrow.png";
 import TravelsContainer from "./components/TravelsContainer/TravelsContainer";
 import UsersContainer from "./components/UsersContainer/UsersContainer";
+import TravelTagsCont from "./components/TravelsContainer/TravelTagsCont/TravelTagsCont";
+import { Box } from "@mui/system";
 
 const TravelsUsersContainer = ({ travelDisplay }) => {
     return (
         <ScrollMenu>
-            {travelDisplay ? <TravelsContainer /> : <UsersContainer />}
-
+            {travelDisplay ? <TravelsContainer /> 
+                            : <UsersContainer />}
+        
         </ScrollMenu>
     );
 };
