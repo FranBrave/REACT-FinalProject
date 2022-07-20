146d32d47bf40d755b82fd9a5922368d5b111224
Francisco José Valiente <gunko360@gmail.com>
2022-07-20 22:41:58 +0200

    home by tags and horizontal scroll :)

--- src/pages/Home/components/TravelsUsersContainer/components/TravelsContainer/TravelsContainer.jsx
+++ src/pages/Home/components/TravelsUsersContainer/components/TravelsContainer/TravelsContainer.jsx
@@ -1,33 +1,32 @@
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import React, { useEffect } from "react";
 import { setReduxTravelsList } from "../../../../../../state/redux/actions/travelActions";
-import PlayaTravelList from "./components/PlayaTravelList/PlayaTravelList";
+import TravelList from "./components/TravelList/TravelList";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 
 const TravelsContainer = () => {
-    const { travelsList } = useSelector((state) => state.travel);
-    const dispatch = useDispatch();
+  const { travelsList } = useSelector((state) => state.travel);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(setReduxTravelsList());
-    }, []);
-    return (
-        <Grid
-            container
-            spacing={1}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            gap="1rem"
-        >
-            <PlayaTravelList travelsList={travelsList} />;
-            {/* <CiudadTravelList />;
-            <InterTravelList />;
-            <NatuTravelList />;
-            <NacionalTravelList />; */}
-        </Grid>
-    );
+  useEffect(() => {
+    dispatch(setReduxTravelsList());
+  }, []);
+
+  return (
+    <Box>
+      <h1>Playa</h1>
+      <TravelList travelsList={travelsList} tipo="Playa" />
+      <h1>Nacional</h1>
+      <TravelList travelsList={travelsList} tipo="Nacional" />
+      <h1>Ciudad</h1>
+      <TravelList travelsList={travelsList} tipo="Ciudad" />
+      <h1>Internacional</h1>
+      <TravelList travelsList={travelsList} tipo="Internacional" />
+      <h1>Naturaleza</h1>
+      <TravelList travelsList={travelsList} tipo="Naturaleza" />
+    </Box>
+  );
 };
 
 export default TravelsContainer;
