7a37b0f387b47e90d55ad2a601c4adc8db9022ea
Jaime <jaimegm93@gmail.com>
2022-07-23 10:12:00 +0200

    trabajo sobre botones join/follow

--- src/pages/TravelDetail/components/ButtonsTravelDetailContainer/ButtonsTravelDetailContainer.jsx
+++ src/pages/TravelDetail/components/ButtonsTravelDetailContainer/ButtonsTravelDetailContainer.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Typography, Grid } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setReduxUserDetail } from "../../../../state/redux/actions/userActions";
+import { setReduxTravelDetail } from "../../../../state/redux/actions/travelActions";
+import { useParams } from "react-router-dom";
+
 
 const ButtonsTravelDetailContainer = () => {
 
+
+
+  const {user} = useSelector((state) => state.user);
+ 
+  //user
+  
+  const dispatchUser = useDispatch();
+
+  useEffect(() => {
+    dispatchUser(setReduxUserDetail());
+  },[]);
+
+  useEffect(() => {}, [user]);
+
+  //travel
+  
+  const { id } = useParams();
+  const BASEURL = "https://viajes-upgrade-hub.herokuapp.com";
+  const TRAVELURL = "/travel/detail/";
+
+  const [travel, setTravel] = useState();
+
+  useEffect(() => {
+    fetch(`${BASEURL}${TRAVELURL}${id}`)
+      .then((response) => response.json())
+      .then((data) => setTravel(data));
+  }, [id]);
+
+  console.log(travel)
+
+  //usersFollowing
+
   
 
-  const Join = () => {
 
+  const Join = () => {const 
+    [usersWantJoin, setUsersWantJoin] = useState();
   };
+    
 
   const Follow = () => {};
 
